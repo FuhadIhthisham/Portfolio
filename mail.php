1c9a0e9d8e3a17e353e3d82d541c587e789faf04
Fuhad Ihthisham <ihthisham.v@gmail.com>
2021-09-22 18:46:45 +0530

    initial

--- mail.php
+++ mail.php
@@ -5,53 +5,42 @@
 		Server-side data validation is also added for good data validation.
 	*/
 	
-	// $data['error'] = false;
+	$data['error'] = false;
 	
-	// $name = $_POST['name'];
-	// $email = $_POST['email'];
-	// $phone = $_POST['phone'];
-	// $website = $_POST['website'];
-	// $message = $_POST['message'];
-	
-	// if( empty($name) ){
-	// 	$data['error'] = 'Please enter your name.';
-	// }else if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
-	// 	$data['error'] = 'Please enter a valid email address.';
-	// }else if( empty($message) ){
-	// 	$data['error'] = 'The message field is required!';
-	// }else if( empty($phone) ){
-	// 	$data['error'] = 'Please enter your phone number.';
-	// }else if ( empty($website) ){
-	// 	$data['error'] = 'Please enter your website.';
-	// }else{
-		
-	// 	$formcontent="From: $name\nPhone: $phone\nWebsite: $website\nEmail: $email\nMessage: $message";
-		
-		
-	// 	//Place your Email Here
-	// 	$recipient = "ihthisham.v@gmail.com";
-		
-	// 	$mailheader = "From: $email \r\n";
-		
-	// 	mail($recipient, $name, $formcontent, $mailheader);
-	// }
-	
-	// echo json_encode($data);
-
-
-
 	$name = $_POST['name'];
-	$email= $_POST['email'];
-	$message= $_POST['message'];
-	$to = "ihthisham.v@gmail.com";
-	$subject = "Mail From website";
-	$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-	$headers = "From: " . $email. "\r\n" .
-	"CC: somebodyelse@example.com";
-	if($email!=NULL){
-		mail($to,$subject,$txt,$headers);
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$website = $_POST['website'];
+	$message = $_POST['message'];
+	
+	if( empty($name) ){
+		$data['error'] = 'Please enter your name.';
+	}else if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
+		$data['error'] = 'Please enter a valid email address.';
+	}else if( empty($message) ){
+		$data['error'] = 'The message field is required!';
+	}else if( empty($phone) ){
+		$data['error'] = 'Please enter your phone number.';
+	}else if ( empty($website) ){
+		$data['error'] = 'Please enter your website.';
+	}else{
+		
+		$formcontent="From: $name\nPhone: $phone\nWebsite: $website\nEmail: $email\nMessage: $message";
+		
+		
+		//Place your Email Here
+		$recipient = "ihthisham.v@gmail.com";
+		
+		$mailheader = "From: $email \r\n";
+		
+		if( mail($recipient, $name, $formcontent, $mailheader) == false ){
+			$data['error'] = 'Sorry, an error occured!';
+		}else{
+			$data['error'] = false;
+		}
+	
 	}
-	//redirect
-	header("Location:thankyou.html");
+	
+	echo json_encode($data);
 	
 ?>
