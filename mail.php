4c2a6157adcf054b067a5ec047d31e4b4f154e5a
Fuhad Ihthisham <ihthisham.v@gmail.com>
2021-09-22 18:45:23 +0530

    mail fix

--- mail.php
+++ mail.php
@@ -5,37 +5,53 @@
 		Server-side data validation is also added for good data validation.
 	*/
 	
-	$data['error'] = false;
+	// $data['error'] = false;
 	
+	// $name = $_POST['name'];
+	// $email = $_POST['email'];
+	// $phone = $_POST['phone'];
+	// $website = $_POST['website'];
+	// $message = $_POST['message'];
+	
+	// if( empty($name) ){
+	// 	$data['error'] = 'Please enter your name.';
+	// }else if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
+	// 	$data['error'] = 'Please enter a valid email address.';
+	// }else if( empty($message) ){
+	// 	$data['error'] = 'The message field is required!';
+	// }else if( empty($phone) ){
+	// 	$data['error'] = 'Please enter your phone number.';
+	// }else if ( empty($website) ){
+	// 	$data['error'] = 'Please enter your website.';
+	// }else{
+		
+	// 	$formcontent="From: $name\nPhone: $phone\nWebsite: $website\nEmail: $email\nMessage: $message";
+		
+		
+	// 	//Place your Email Here
+	// 	$recipient = "ihthisham.v@gmail.com";
+		
+	// 	$mailheader = "From: $email \r\n";
+		
+	// 	mail($recipient, $name, $formcontent, $mailheader);
+	// }
+	
+	// echo json_encode($data);
+
+
+
 	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$phone = $_POST['phone'];
-	$website = $_POST['website'];
-	$message = $_POST['message'];
-	
-	if( empty($name) ){
-		$data['error'] = 'Please enter your name.';
-	}else if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
-		$data['error'] = 'Please enter a valid email address.';
-	}else if( empty($message) ){
-		$data['error'] = 'The message field is required!';
-	}else if( empty($phone) ){
-		$data['error'] = 'Please enter your phone number.';
-	}else if ( empty($website) ){
-		$data['error'] = 'Please enter your website.';
-	}else{
-		
-		$formcontent="From: $name\nPhone: $phone\nWebsite: $website\nEmail: $email\nMessage: $message";
-		
-		
-		//Place your Email Here
-		$recipient = "ihthisham.v@gmail.com";
-		
-		$mailheader = "From: $email \r\n";
-		
-		mail($recipient, $name, $formcontent, $mailheader);
+	$email= $_POST['email'];
+	$message= $_POST['message'];
+	$to = "ihthisham.v@gmail.com";
+	$subject = "Mail From website";
+	$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+	$headers = "From: " . $email. "\r\n" .
+	"CC: somebodyelse@example.com";
+	if($email!=NULL){
+		mail($to,$subject,$txt,$headers);
 	}
-	
-	echo json_encode($data);
+	//redirect
+	header("Location:thankyou.html");
 	
 ?>
